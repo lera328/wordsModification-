093b87b70dff07cd78e32276b224d4a408fa7108
lera328 <128339211+lera328@users.noreply.github.com>
2023-06-27 10:03:26 +0300

    Add files via upload

---
+++ готово/WpfApp1/Helper.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Helper
+    {
+        public string Valid(string word1, string word2)
+        {
+            if (word1==""||word2=="")
+            {
+                return "Поля не заполнены";
+            }
+
+            if (word1.Length!=word2.Length)
+            {
+                return "Слова не могут быть разной длины!";
+            }
+
+            string pattern="[а-яА-Я]+$";
+            
+            if (!Regex.IsMatch(word1,pattern) || !Regex.IsMatch(word2, pattern))
+            {
+                return "Ошибка ввода: слова должны содержать только буквы русского алфавита";
+            }
+            if (word1.Contains(" ") || word2.Contains(" ")) return "В каждом поле должно содержаться только одно слово";
+            return null;
+        }
+
+        private List<string> GetCondidates(List<string> distionary, string word)
+        {
+            int len = word.Length;
+            List<string> condidates = new List<string>();
+            for(int i = 0; i < distionary.Count; i++)
+            {
+                if(distionary.ElementAt(i).Length== len)
+                {
+                    condidates.Add(distionary.ElementAt(i));
+                }
+            }
+            return condidates;
+        }
+        // Функция для проверки, можно ли получить новое слово из старого, меняя одну букву
+        private bool CanTransform(string word1, string word2)
+        {
+            int count = 0;
+
+            for (int i = 0; i < word1.Length; i++) //слова должны одной буквой отличаться!!
+            {
+                if (word1[i] != word2[i])
+                {
+                    count++;
+                }
+            }
+
+            if (count == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        
+        public List<string> Find(string startWord, string endWord, List<string> words) //начальное слово, конечное слово и словарь
+        {
+            
+            words = GetCondidates(words, startWord);
+            var visited = new List<string>();// Создаем лист посещенных слов
+
+            
+            List<List<string>> list = new List<List<string>>();// Создаем лист листов ...
+
+            
+            list.Add(new List<string> { startWord });// Добавляем в лист лист, содержащий только начальное слово
+
+            
+            while (list.Count > 0) // Пока в листе что-то есть
+            {
+                // Получаем лист1 из листа
+                var path = list.ElementAt(0);
+
+                // Удаляем лист1 из листа
+                list.Remove(path);
+
+                // Получаем последнее слово в лист1
+                string word = path.ElementAt(path.Count - 1);
+
+                // Если это конечное слово, возвращаем список слов
+                if (word == endWord)
+                    return path;
+
+                // Если этого слова еще нет в списке посещенных
+                if (!visited.Contains(word))
+                {
+                    // Добавляем его в список посещенных 
+                    visited.Add(word);
+
+                    // перебираем слова в листе words (в нашем словаре)
+                    foreach (var nextWord in words)
+                    {
+                        // Если есть подходящее нам слово и оно не содержится в лист1
+                        if (CanTransform(word, nextWord) && !path.Contains(nextWord))
+                        {
+                            // Создаем новый лист2, копируя все элементы из старого списка, добавляем туда найденное слово
+                            var newPath = new List<string>(path);
+                            newPath.Add(nextWord);
+
+                            // Добавляем новый лист2 в основной лист
+                            list.Add(newPath);
+                        }
+                    }
+                }
+            }
+            // Если цепочка не найдена, возвращаем null
+            return null;
+        }
+    }
+}
---
+++ готово/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {//для тестов ночь — день, друг — враг, коза —волк и кот-пес.
+        
+       List <string> distionary;
+        Helper a;
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbStartWord.Focus();
+            // Загружаем словарь
+            var lines = new HashSet<string>(File.ReadAllLines(@"Distionary\freqrnc2011_s.csv"));
+            distionary = new List<string>();
+            foreach (var line in lines)
+            {
+                var word = line.Split(',')[0];
+                distionary.Add(word);
+            }
+            
+
+        }
+
+        private void btSearch_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            //засекаем время начала операции
+            stopwatch.Start();
+            a = new Helper();
+            var startWord = tbStartWord.Text.ToLower();//берем слова, все буквы в один регистр
+            var endWord = tbEndWord.Text.ToLower();
+            string wor = a.Valid(startWord, endWord);
+            if (wor == null)
+            {
+                List<string> chain = a.Find(startWord, endWord, distionary);
+
+                if (chain != null)
+                {
+                   // lbResult.Content = "РЕЗУЛЬТАТ";
+                    tbResult.Text = string.Join(" -> ", chain);
+                }
+                else
+                    tbResult.Text = "Цепочка не найдена";
+            }
+            else MessageBox.Show(wor);
+            stopwatch.Stop();
+            lbResult.Content = "РЕЗУЛЬТАТ (Время поиска "+Convert.ToDouble(stopwatch.ElapsedMilliseconds)/1000+" с)";
+            tbStartWord.Clear();
+            tbEndWord.Clear();
+            tbStartWord.Focus();
+
+        }
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Автор: Шульгина В.И. ПИН-221");
+        }
+
+        private void Instructions_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Инструкция:\n1. Введите слово-начало цепи мутации \n2. Введите слово-конец цепи\n3. Нажмите на кнопку и через некоторое время будет сгенерирована цепь");
+        }
+
+        private void TbStartWord_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) tbEndWord.Focus();
+        }
+
+        private void TbEndWord_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter) btSearch_Click(sender,e);
+
+        }
+
+       
+
+        private void TbStartWord_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            tbStartWord.Text="";
+
+        }
+
+        private void TbEndWord_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            tbEndWord.Text="";
+
+        }
+        //private void Exit_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Application.Current.Shutdown();
+        //}
+
+
+
+
+    }
+}
